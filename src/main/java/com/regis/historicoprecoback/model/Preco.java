259f13b66d334fa3dd8c6509ad9584cc55379918
Regis Yamamoto <regisyamamoto@gmail.com>
2023-01-15 00:43:40 -0300

    add jakarta validator

--- src/main/java/com/regis/historicoprecoback/model/Preco.java
+++ src/main/java/com/regis/historicoprecoback/model/Preco.java
@@ -2,7 +2,6 @@ package com.regis.historicoprecoback.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -19,15 +18,12 @@ public class Preco implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id = 0L;
 
-    @NotNull
     @Column(name = "preco", nullable = false)
     private BigDecimal preco = new BigDecimal(0.0);
 
-    @NotNull
     @Column(name = "data_consulta", nullable = false)
     private LocalDateTime dataConsulta = LocalDateTime.now(ZoneId.of("America/Sao_Paulo"));
 
-    @NotNull
     @Column(name = "loja_consultada", nullable = false)
     private String lojaConsultadada = "";
 
